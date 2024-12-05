6d569d372cc7e363fa22a1eb9469de3f6448d2be
Guilherme Johann <guilherme4695@gmail.com>
2024-12-05 10:34:43 -0300

    OrderItem, many-to-many association with extra attributes part 2- ue porra

--- src/main/java/com/johann/webservicespringboot/config/TestConfig.java
+++ src/main/java/com/johann/webservicespringboot/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.johann.webservicespringboot.config;
 
-import com.johann.webservicespringboot.entities.Category;
-import com.johann.webservicespringboot.entities.User;
-import com.johann.webservicespringboot.entities.Order;
-import com.johann.webservicespringboot.entities.Product;
+import com.johann.webservicespringboot.entities.*;
 import com.johann.webservicespringboot.entities.enums.OrderStatus;
-import com.johann.webservicespringboot.repositories.CategoryRepository;
-import com.johann.webservicespringboot.repositories.OrderRepository;
-import com.johann.webservicespringboot.repositories.ProductRepository;
-import com.johann.webservicespringboot.repositories.UserRepository;
+import com.johann.webservicespringboot.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -65,6 +62,11 @@ public class TestConfig implements CommandLineRunner {
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 
     }
--- src/main/java/com/johann/webservicespringboot/repositories/OrderItemRepository.java
+++ src/main/java/com/johann/webservicespringboot/repositories/OrderItemRepository.java
@@ -1,7 +1,8 @@
 package com.johann.webservicespringboot.repositories;
 
+import com.johann.webservicespringboot.entities.OrderItem;
 import com.johann.webservicespringboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
 }
